a067f5a641290cfb8f930f177277675b9a1bc511
Алина Радюкевич <alina.makaroshka@outlook.com>
2023-02-27 21:47:55 +0300

    add a GuessingGame class implementation

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.centre = Math.round((this.min + this.max) / 2 );
+        return this.centre;     
     }
 
     lower() {
-
+        this.max = this.centre;
     }
 
     greater() {
-
+        this.min = this.centre;
     }
 }
 
